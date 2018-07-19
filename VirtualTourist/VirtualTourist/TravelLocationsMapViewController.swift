d5778fd0c6a25671029efe4eec9fda06fea6eb30
Bernadett Kiss <info.bernadettkiss@gmail.com>
2018-07-19 14:04:41 -0400

    feat: Add edit mode and delete pin

--- VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
@@ -13,11 +13,17 @@ import CoreData
 class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var instructionView: UIView!
+    @IBOutlet weak var instructionLabel: UILabel!
     
     var dataController: DataController!
     
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = editButtonItem
         
         let fetchRequest: NSFetchRequest<Pin> = Pin.fetchRequest()
         if let result = try? dataController.viewContext.fetch(fetchRequest) {
@@ -30,18 +36,31 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
         mapView.addGestureRecognizer(longPressGestureRecognizer)
     }
     
-    @objc func handleLongPress(_ sender: UILongPressGestureRecognizer) {
-        if sender.state == .ended {
-            let touchPoint = sender.location(in: mapView)
-            let coordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-            addPin(atCoordinates: coordinates)
+    // MARK: - Actions
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        if editing {
+            instructionView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            instructionLabel.text = "Tap pins to delete"
+        } else {
+            instructionView.backgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+            instructionLabel.text = "Long-press on the map to drop a pin"
         }
     }
     
-    func addPin(atCoordinates coordinates: CLLocationCoordinate2D) {
+    @objc func handleLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .ended {
+            let touchPoint = sender.location(in: mapView)
+            let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            addPin(atCoordinate: coordinate)
+        }
+    }
+    
+    func addPin(atCoordinate coordinate: CLLocationCoordinate2D) {
         let pin = Pin(context: dataController.viewContext)
-        pin.latitude = coordinates.latitude
-        pin.longitude = coordinates.longitude
+        pin.latitude = String(coordinate.latitude)
+        pin.longitude = String(coordinate.longitude)
         try? dataController.viewContext.save()
         createAnnotation(for: pin)
     }
@@ -50,13 +69,41 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
         for pin in pins {
             createAnnotation(for: pin)
         }
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
     func createAnnotation(for pin: Pin) {
-        let coordinates = CLLocationCoordinate2D(latitude: CLLocationDegrees(pin.latitude), longitude: CLLocationDegrees(pin.longitude))
+        let latitude = CLLocationDegrees(pin.latitude!)
+        let longitude = CLLocationDegrees(pin.longitude!)
+        let coordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
         let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinates
+        annotation.coordinate = coordinate
         mapView.addAnnotation(annotation)
-        mapView.reloadInputViews()
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation else {
+            return
+        }
+        let latitude = String(annotation.coordinate.latitude)
+        let longitude = String(annotation.coordinate.longitude)
+        
+        if isEditing {
+            let fetchRequest: NSFetchRequest<Pin> = Pin.fetchRequest()
+            let latitudePredicate = NSPredicate(format: "latitude = %@", latitude)
+            let longitudePredicate = NSPredicate(format: "longitude = %@", longitude)
+            let subpredicates: [NSPredicate]
+            subpredicates = [latitudePredicate, longitudePredicate]
+            let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: subpredicates)
+            fetchRequest.predicate = compoundPredicate
+            if let result = try? dataController.viewContext.fetch(fetchRequest) {
+                if !result.isEmpty {
+                    let pinToDelete = result[0]
+                    dataController.viewContext.delete(pinToDelete)
+                    try? dataController.viewContext.save()
+                }
+            }
+            mapView.removeAnnotation(annotation)
+        }
     }
 }
